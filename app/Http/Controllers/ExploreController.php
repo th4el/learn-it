56f2704e65270f88fd951ca014a60aeb92a54c2b
th4el <alfiftah@gmail.com>
2025-12-08 20:10:08 +0700

    Menambahkan Explore Controller

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -9,7 +9,7 @@ class ExploreController extends Controller
 {
     public function explore(){
         $sd = Grade::whereBetween('id', [1,6])->get();
-        $mp = Grade::whereBetween('id', [7,9])->get();
+        $smp = Grade::whereBetween('id', [7,9])->get();
         $sma = Grade::whereBetween('id', [10,12])->get();
 
         return view('explore', compact('sd', 'smp', 'sma'));
